258e91f5eaebb02703f125e1db6e6b8394d0ecdc
Andebugulin <gulin.andrey2005@gmail.com>
2024-03-10 12:39:37 +0200

    feature: added-manage_db-command-to-the-script-to-automate-database-cli
    now using <./start-service manage_db> command you will get access to the database via terminal

--- start-service.sh
+++ start-service.sh
@@ -9,6 +9,7 @@ function help() {
     echo "usage:"
     echo "  run:"
     echo "    $0 db               - start PostgreSQL database"
+    echo "    $0 manage_db        - start a PostgreSQL terminal session"
     echo "  stop:"
     echo "    $0 stop             - stop all docker-services"
 }
@@ -23,6 +24,18 @@ function stop_services() {
     docker-compose -f ./database/postgres.yml down
 }
 
+function manage_db() {
+    # Check if the database container is running
+    db_container=$(docker-compose -f ./database/postgres.yml ps -q db)
+    if [ -z "$db_container" ] || ! docker ps -q --no-trunc | grep -q "^$db_container$"; then
+        echo "Database container is not running. Run 'db' first before accessing the data."
+        return
+    fi
+
+    echo "Starting PostgreSQL terminal session..."
+    docker-compose -f ./database/postgres.yml exec db psql -U postgres
+}
+
 # Check if the script is run from the git root directory
 if [[ $PWD != $(git rev-parse --show-toplevel) ]]; then
     echo "This script must be run from the root directory of the repository"
@@ -33,6 +46,7 @@ command=${1:-}
 
 case $command in
     db) start_database ;;
+    manage_db) manage_db ;;
     stop) stop_services ;;
     -h) help ;;
     --help) help ;;
