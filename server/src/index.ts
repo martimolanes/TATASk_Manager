5375cf2b5c10e58f2122bd09b87a763f99f45189
martinho <martinho.n03@gmail.com>
2024-04-14 20:38:51 +0300

    feat(server): added json support para express

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import activityRouter from './routes/activity';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 const PORT: number = Number(process.env.PORT) || 3333;
 
